06ec9161cc3a393388820e0dc0f3ac49e8d9d01a
Shanwi B M <shanwibm@gmail.com>
2025-05-29 00:01:01 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import './components/styles.css';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+)
+
